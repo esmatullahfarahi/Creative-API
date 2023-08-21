bc76542eb8da935f939d68cb097c389982721840
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-08-21 16:59:20 -0400

    Index number and delete option for stored items

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ fetchFactBtn.addEventListener("click", async () => {
   // Function to display a fact on the page
 const displayFact = (fact) => {
   const factDisplay = document.getElementById("factDisplay");
-  factDisplay.innerHTML = fact ? `<b>Did you know</b> "${fact.fact}"` : "No facts available.";
+  factDisplay.innerHTML = fact ? `<b>Did you know?</b> "${fact.fact}"` : "No facts available.";
 };
 
 
@@ -60,15 +60,44 @@ const updateStoredFactsUI = () => {
   const storedFacts =
     JSON.parse(localStorage.getItem("randomFacts")) || [];
 
-  // Display stored facts in reverse order (latest first)
-  storedFacts.reverse().forEach((fact, index) => {
+// Add "Delete All" button
+  const deleteAllButton = document.createElement("button");
+  deleteAllButton.textContent = "Reset All";
+  deleteAllButton.classList.add("delete-all-button");
+  deleteAllButton.addEventListener("click", () => deleteAllFacts());
+  storedFactsList.appendChild(deleteAllButton);
+
+    // Display stored facts in reverse order (latest first)
+    storedFacts.reverse().forEach((fact, index) => {
     const listItem = document.createElement("li");
     listItem.classList.add("stored-fact");
 
+    // Create a paragraph element for the fact text
     const factText = document.createElement("p");
-    factText.textContent = fact.fact;
-
+    factText.textContent = `${index + 1}. ${fact.fact}`;
     listItem.appendChild(factText);
     storedFactsList.appendChild(listItem);
+
+       // Add delete single item button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", () => deleteFact(index));
+    listItem.appendChild(deleteButton);
   });
+};
+
+// Function to delete a stored fact from local storage
+const deleteFact = (index) => {
+  const storedFacts =
+    JSON.parse(localStorage.getItem("randomFacts")) || [];
+  storedFacts.splice(index, 1);
+  localStorage.setItem("randomFacts", JSON.stringify(storedFacts));
+  updateStoredFactsUI();
+};
+
+// Function to delete all stored facts from local storage
+const deleteAllFacts = () => {
+  localStorage.removeItem("randomFacts");
+  updateStoredFactsUI();
 };
